76b46a4c0416bb4426b23b7a699898f6b8ccf9ac
Shafeeque <skhan@fiu.edu>
2022-11-19 21:21:57 -0500

    Adding project

---
+++ src/main/java/com/pickers/moviepicker/MoviePickerApplication.java
@@ -0,0 +1,13 @@
+package com.pickers.moviepicker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviePickerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviePickerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pickers/moviepicker/MoviePickerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pickers.moviepicker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviePickerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
